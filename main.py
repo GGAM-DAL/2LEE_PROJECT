890c6dabe2c7069459086489d4263310de13cf8b
ggamdal <enjung1997@gmail.com>
2023-04-26 20:08:59 +0900

    fork

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url1 = "https://github.com/GGAM-DAL/2LEE_PROJECT/blob/c311f7ef8805658141787749db9dd70b93091b02/population.csv"
 
 def chart(df, region, year):
     df.loc[region, year].plot(kind='pie',
@@ -13,14 +12,14 @@ def chart(df, region, year):
     plt.legend(df['유형'])
     plt.show()
 
-region_input = Element("region_input")
-result = Element("result")
+# region_input = Element("region_input")
+region_input = Element(region_input)
 def plot1(*args):
     region = region_input.value
-    df_ = pd.read_csv(url1)
+    df_ = pd.read_csv("population.csv", encoding='cp949')
     df = df_[['자치구', '유형', '2013', '2014', '2015','2016','2017', '2018', '2019', '2020', '2021']]
     df.set_index('자치구', drop=True,inplace=True)
-    result = chart(df, region, '2013')
+    chart(df, region, '2013')
     
 
 if __name__ == '__main__':
