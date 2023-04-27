1c7a74d7780003e0651218311a06159175e2e2ef
ggamdal <enjung1997@gmail.com>
2023-04-27 23:54:29 +0900

    올라가라고

--- LEJ_PY.py
+++ LEJ_PY.py
@@ -4,24 +4,26 @@ import matplotlib.pyplot as plt
 from xml.etree.ElementTree import Element
 
 
+
 def chart(df, region, year):
     df.loc[region, year].plot(kind='pie',
                               autopct='%.3f%%',
                               startangle=10,
                               colors=['#993399','#DCDCDC'],
                               textprops={'size':15, 'color':'white', 'weight':'bold'})
-    plt.show()
+    # result.element = plt.show()
 
-region_input = Element("region_input")
-def plot1():
-    region = region_input
-    url1 = "https://raw.githubusercontent.com/GGAM-DAL/2LEE_PROJECT/main/population.csv"
-    df = pd.read_csv(url1, encoding='cp949')
-    df.set_index('자치구', drop=True,inplace=True)
-    print(df)
-    # chart(df, region, '2013')
+def plot1(*args):
+    region = document.getElementById("region_input").value
+    result = Element("result")
 
-    
+
+    result.element.innerText = print(f'선택한 지역은 {region}')
+    region_input.clear()
+    # url1 = "https://raw.githubusercontent.com/GGAM-DAL/2LEE_PROJECT/main/population.csv"
+    # df = pd.read_csv(url1, encoding='cp949')
+    # df.set_index('자치구', drop=True,inplace=True)
+
 
 if __name__ == '__main__':
     plot1()
